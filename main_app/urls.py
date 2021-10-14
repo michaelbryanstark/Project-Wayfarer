c52bd8f5d464948b72a346ac6438dde83238298e
Jonathan Flores <jonathanflores@hotmail.com>
2021-10-13 18:37:58 -0700

    added signup url and auth

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 # this like app.use() in express
 urlpatterns = [
  path('', views.Home.as_view(), name="home"),
+ path('accounts/signup/', views.Signup.as_view(), name="signup"),
+ path('accounts/profile', views.Profile.as_view(), name="profile"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,9 +2,28 @@ from django.shortcuts import render
 from django.views import View # <- View class to handle requests
 from django.http import HttpResponse # <- a class to handle sending a type of response
 from django.views.generic.base import TemplateView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
 
 class Home(TemplateView):
     template_name = "home.html"
+    
+    
+    
+class Signup(view):
+    def get(self, request):
+        form = UserCreationForm()
+        context = {"form": form}
+        return render(request, 'registration/signup.html', context)
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user.username)
+            return redirect('/')
+        else:
+            context = {'form': form}
+            return render(request, 'registration/signup.html', context)
