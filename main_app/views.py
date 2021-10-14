3cca2c68880c2faf8662758b041ca16bc17b0673
Jonathan Flores <jonathanflores@hotmail.com>
2021-10-13 18:45:22 -0700

    imported redirect in views

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View # <- View class to handle requests
 from django.http import HttpResponse # <- a class to handle sending a type of response
 from django.views.generic.base import TemplateView
