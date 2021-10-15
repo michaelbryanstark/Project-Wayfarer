815047941b2baa354865d8cb163234825da9989c
brian kasten <panphobicdream@gmail.com>
2021-10-14 20:13:18 -0400

    profile page works, and is where it redirects when you login, and if you try to access profile without logging in, it boots you to the login page

--- main_app/views.py
+++ main_app/views.py
@@ -4,17 +4,16 @@ from django.http import HttpResponse # <- a class to handle sending a type of re
 from django.views.generic.base import TemplateView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 # Create your views here.
-
 class Home(TemplateView):
     template_name = "home.html"
-    
+@method_decorator(login_required, name='dispatch')
 class Profile(TemplateView):
     template_name = "profile.html"
-    
-    
+
 class Signup(View):
     def get(self, request):
         form = UserCreationForm()
@@ -25,7 +24,7 @@ class Signup(View):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('/')
+            return redirect('/profile/')
         else:
             context = {'form': form}
             return render(request, 'registration/signup.html', context)
