f28258dd00e7a5cf43bef640820f30da46ce8e33
dev-slater <83676602+dev-slater@users.noreply.github.com>
2021-10-16 14:03:36 -0400

    Revert "modified profile page"

--- main_app/views.py
+++ main_app/views.py
@@ -4,15 +4,10 @@ from django.http import HttpResponse # <- a class to handle sending a type of re
 from django.views.generic.base import TemplateView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.views.generic.edit import CreateView, UpdateView
-from .models import Profile
-from django import forms
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 
-
 # Create your views here.
-
 class Home(TemplateView):
     template_name = "home.html"
 @method_decorator(login_required, name='dispatch')
@@ -33,62 +28,3 @@ class Signup(View):
         else:
             context = {'form': form}
             return render(request, 'registration/signup.html', context)
-          
-class ProfileForm(forms.ModelForm):
-    model = Profile
-    fields = [
-        'name',
-        'current_city',
-        'past_cities'
-    ]
-
-
-# def profile(request):
-#     # posts = Posts.objects.filter(author=request.user.id)
-#     # profile = Profile.objects.get(user=request.user)
-#     # comments = Comment.objects.filter(author=request.user.id)
-
-#     context = {
-#         'profile': profile,
-#         # 'posts': posts,
-#         # 'comments': comments,
-#         # 'num_comments': len(comments),
-#         # 'num_posts': len(posts)
-#     }
-#     return render(request, 'profiles/index.html', context)       
-
-# def add_profile(request):
-#     error_message = ''
-#     if request.method == 'POST':
-#         profile_form = ProfileForm(request.POST)
-#         if profile_form.is_valid():
-#             new_profile = profile_form.save(commit=False)
-#             new_profile.user_id = request.user.id
-#             new_profile.save()
-#             return redirect('profile')
-#         else:
-#             error_message = 'Something went wrong - try again'
-#     else:
-#         profile_form = ProfileForm()
-#         context = {
-#             'profile_form': profile_form, 
-#             'error_message': error_message
-#         }
-#         return render(request, 'profiles/add.html', context)
-    
-# def update_profile(request):
-#     error_message = ''
-#     if request.method == 'POST':
-#         profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
-#         if profile_form.is_valid():
-#             updated_profile = profile_form.save()
-#             return redirect('profile')
-#         else:
-#             error_message = 'Something went wrong - try again'
-#     else:
-#         profile_form = ProfileForm(instance=request.user.profile)
-#         context = {
-#             'profile_form': profile_form, 
-#             'error_message': error_message
-#         }
-#         return render(request, 'profiles/edit.html', context)
