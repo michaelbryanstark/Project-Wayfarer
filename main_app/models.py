c7674c107508abccd9fecea6b4fd41effdeb9a1b
buhari.maiwada <maewada@live.com>
2021-10-16 11:59:51 -0400

    modified model.py

--- main_app/models.py
+++ main_app/models.py
@@ -3,10 +3,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-# class UserProfile(models.Model):
-#     city = models.CharField(max_length=100)
-#     img = models.CharField(max_length=250, default=None)
-#     description = models.TextField(max_length=1000)
 
 class Profile(models.Model):
     name = models.CharField(max_length=50)
@@ -14,7 +10,7 @@ class Profile(models.Model):
     join_date = models.DateField(auto_now_add=True)
     past_cities = models.TextField(blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # pic = models.ImageField(upload_to="main_app/static/images", null=True)
+  
 
     def __str__(self):
         return self.name
