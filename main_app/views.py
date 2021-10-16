d5b09e90a5de25e99e640a07fcba1ca740894e43
Matthew Slater <slaterm100@gmail.com>
2021-10-16 15:45:50 -0400

    Cities route, display and post fake data

--- main_app/views.py
+++ main_app/views.py
@@ -28,3 +28,44 @@ class Signup(View):
         else:
             context = {'form': form}
             return render(request, 'registration/signup.html', context)
+
+class City:
+    def __init__(self, image, name):
+        self.image = image
+        self.name = name
+        
+class CityList(TemplateView):
+    template_name = "city_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["cities"] = cities
+        context["posts"] = posts
+        return context
+    
+cities = [
+    City("https://cdn.shopify.com/s/files/1/2012/8501/products/80775098-b507-481e-bada-412f5600ae06_61e2bacb-72c2-451d-91ae-3d4783de5b81_256x256.jpg?v=1628179761","New York"),
+    City("https://cdn.shopify.com/s/files/1/2012/8501/products/80775098-b507-481e-bada-412f5600ae06_61e2bacb-72c2-451d-91ae-3d4783de5b81_256x256.jpg?v=1628179761","Atlanta"),
+]
+       
+class Post:
+    def __init__(self, title, image, text, date_created, author, city):
+        self.title = title
+        self.image = image
+        self.text = text
+        self.date_created = date_created
+        self.author = author #FK 
+        self.city = city #FK
+
+# class PostList(TemplateView):
+#     template_name = "city_list.html"
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context["posts"] = posts
+#         return context
+    
+posts = [
+    Post("Welcome to New York","https://cdn.shopify.com/s/files/1/2012/8501/products/80775098-b507-481e-bada-412f5600ae06_61e2bacb-72c2-451d-91ae-3d4783de5b81_256x256.jpg?v=1628179761","This is a test post","1/1/2021","John Smith","New York"),
+     Post("Welcome to New York","https://cdn.shopify.com/s/files/1/2012/8501/products/80775098-b507-481e-bada-412f5600ae06_61e2bacb-72c2-451d-91ae-3d4783de5b81_256x256.jpg?v=1628179761","This is a test post","1/1/2021","John Smith","New York"),
+]
